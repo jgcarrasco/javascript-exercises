e9d3be6d4ee76e51c207d94d0ba0713a84e1b840
Jorge GC <jorgarcar97@gmail.com>
2024-06-06 12:07:25 +0200

    exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(xs) {
+  return xs.reduce((acc, x) => {
+    return acc += x;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(xs) {
+  return xs.reduce((acc, x) => {
+    return acc *= x;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  if (b === 0) {
+    return 1;
+  } else {
+    let acc = a
+
+    for (let i = 0; i < b - 1; i++) {
+      acc *= a;
+    }
+    return acc;
+  }
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  if (x < 2) { return 1; }
+  else { return x * factorial(x-1); }
 };
 
 // Do not edit below this line
